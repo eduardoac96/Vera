7463e64680e9ed878f04551abcc9bf505285a641
mac <mac@Alvarado-Sandoval.local>
2022-09-14 20:44:41 -0500

    Initial repo upload

---
+++ VeraMobile/veramobile/android/app/src/main/kotlin/com/example/veramobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.veramobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- VeraUI/ClientApp/src/app/models/realState/realState.model.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=realState.model.js.map
